317d55d510c8c39631f6a6499bccc64085bdc0ad
Nino Mauuary <nino.mauuary@smart-origin.com>
2026-01-06 16:01:45 +0100

    [feat] add luasnip keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Minimal essential keymaps only
 
 local map = vim.keymap.set
+local ls = require("luasnip")
 
 -- Copy entire buffer to clipboard
 map("n", "<C-c>", ":%y<CR>", { noremap = true, silent = true, desc = "Copy entire buffer" })
@@ -16,3 +17,18 @@ map("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
 -- Word operations
 map("n", "yw", "yiw", { desc = "Copy word under cursor" })
 map("n", "cw", "ciw", { desc = "Edit word under cursor" })
+
+-- Remap luansip key to jump between snippets node
+map({ "i", "s" }, "<C-D>", function()
+	ls.jump(1)
+end, { silent = true })
+map({ "i", "s" }, "<C-G>", function()
+	ls.jump(-1)
+end, { silent = true })
+
+-- Expand lua snippet
+map({ "i", "s" }, "<C-j>", function()
+	if ls.expandable() then
+		ls.expand()
+	end
+end, { silent = true })
